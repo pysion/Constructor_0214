d77240302d0779dbb24f5680f202a40fcf1bfe4b
AdamRaditya <Adamradityaakhsan@gmail.com>
2025-05-17 17:34:59 +0700

    membuat class mahasiswa

--- MbrStatikfunc.cpp
+++ MbrStatikfunc.cpp
@@ -2,3 +2,18 @@
 #include <string>
 using namespace std;
 
+class mahasiswa{
+private:
+     static int nim;
+public:
+     int id;
+     string nama;
+
+     void setID();
+     void printAll();
+
+     static void setNim(int pNim){nim=pNim; /*Definisi Function*/}
+     static int getnim(){return nim; /*Defiinisi Function*/}
+
+     mahasiswa(string pnama):nama(pnama){setID();}
+};
